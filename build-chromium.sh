0a338e9558956a804141c1f08f71c2e3783a6447
HostZero <haneesh.da.rocker@gmail.com>
2016-07-19 16:05:25 +0530

    dev: fix build fails

--- build-chromium.sh
+++ build-chromium.sh
@@ -1,4 +1,7 @@
 echo
+CLONED_DIR= /home/hostzero/chromium
+OUT_DIR= /home/hostzero/chromium/out
+OUT= /home/hostzero/chromium/out/target/product/chromium
 echo Cleaning $OUT directory
 rm -rf out
 echo ==================
@@ -6,20 +9,20 @@ echo Building Chromium
 echo ==================
 echo
 echo Starting Build...
-mkdir ChromePublic
+mkdir ext_build
 echo
 echo Requesting Sudo Permission...
-sudo cp -a . /ChromePublic/
-rm -rf ChromePublic/tools/*
-rm -rf ChromePublic/tools
-mkdir out/target/product
+sudo cp -a $CLONED_DIR/. /ext_build/
+rm -rf $CLONED_DOR/tools/*
+rm -rf $CLONED_DIR/tools
+rm -rf $CLONED_DIR/build-chromium.sh
+mkdir $OUT
 echo Compiling....
-java -jar apktool.jar b ChromePublic
-mv ChromePublic/dist/ChromePublic.apk out/com.android.chromium_v1.0-minAPI19(arm,arm64-v8a,armeabi-v7a).apk
-rm -rf ChromePublic
+java -jar apktool.jar b ext_build
+mv $CLONED_DIR/dist/*.apk $OUT/Chromium.apk
+rm -rf ext_build
 echo
 echo ==============================
 echo Build Success
-echo $OUT= out/target/product/com.android.chromium_v1.0-minAPI19(arm,arm64-v8a,armeabi-v7a).apk
 echo ===============================
 
